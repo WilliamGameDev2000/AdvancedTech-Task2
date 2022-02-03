d437246b72e8be53166b7354a58e6ef2eb9dbe14
William <Hamish2.Lawrence@live.uwe.ac.uk>
2022-02-03 14:09:01 +0000

    simplifying goal down for newer appraoch

---
+++ ClayAnimations/Assets/DeformMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeformMesh : MonoBehaviour
+{
+
+    Vector3[] original_vertex_positions;
+    Vector3[] vertex_positions;
+
+    Transform[] bone_collection;
+    void Awake()
+    {
+        original_vertex_positions = gameObject.GetComponentInChildren<SkinnedMeshRenderer>().sharedMesh.vertices;
+        bone_collection = gameObject.GetComponentInChildren<SkinnedMeshRenderer>().bones;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            Debug.Log(bone_collection[0]);
+        }
+        
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ResetVertices();
+        }
+    }
+
+    void ResetVertices()
+    {
+        vertex_positions = original_vertex_positions;
+    }
+}
--- ClayAnimations/Assets/MoveBones.cs
+++ ClayAnimations/Assets/MoveBones.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class MoveBones : MonoBehaviour
@@ -7,18 +8,53 @@ public class MoveBones : MonoBehaviour
     GameObject bone;
     GameObject player;
 
-    [SerializeField] int bonePick;
+   
+
+    //[SerializeField] [Range(0, 16)]int bonePick;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         player = GameObject.Find("SimpleMan/Body");
-        bone = player.GetComponent<SkinnedMeshRenderer>().bones[bonePick].gameObject;
-        Debug.Log(bone);
-    }
 
+            //setBone(bonePick);
+    }
     // Update is called once per frame
     void Update()
     {
-        bone.transform.localEulerAngles = new Vector3(bone.transform.eulerAngles.x + 0.02f * Time.deltaTime, bone.transform.eulerAngles.y + 0.02f * Time.deltaTime, bone.transform.eulerAngles.z + 0.02f * Time.deltaTime);
+        //setBone(bonePick);
+        //bone.transform.localEulerAngles = new Vector3(bone.transform.eulerAngles.x + 0.02f * Time.deltaTime, bone.transform.eulerAngles.y + 0.02f * Time.deltaTime, bone.transform.eulerAngles.z + 0.02f * Time.deltaTime);
     }
+
+  /*  public void setBone(int bonepicked)
+    {
+        bonePick = bonepicked;
+        bone = player.GetComponent<SkinnedMeshRenderer>().bones[bonePick].gameObject;
+    }*/
+
+    /*[ExecuteInEditMode]
+    void OnDrawGizmosSelected()
+    {
+        Handles.color = Color.red;
+        foreach(Transform Bone in player.GetComponent<SkinnedMeshRenderer>().bones)
+        {
+            if (Bone.parent != player.GetComponent<SkinnedMeshRenderer>().bones[0])
+            {
+                Handles.DrawLine(Bone.position, Bone.parent.position);
+            }
+            else
+            {
+                Handles.DrawLine(Bone.GetChild(0).position, Bone.position);
+            }
+        }
+
+        Handles.color = Color.green;
+        if(bonePick != 0)
+        {
+            Handles.DrawLine(bone.transform.position, bone.transform.parent.position);
+        }
+        else
+        {
+            Handles.DrawLine(bone.transform.GetChild(0).position, bone.transform.position);
+        }
+    }*/
 }
